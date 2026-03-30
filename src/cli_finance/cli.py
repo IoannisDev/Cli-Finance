99b9ed8909ed9851924e92fe1d645a4c41e8df3d
IoannisDev <aangphurba101@gmail.com>
2026-03-30 15:39:49 -0500

    Added cli and database logic

--- src/cli_finance/cli.py
+++ src/cli_finance/cli.py
@@ -2,20 +2,46 @@
 
 import typer
 from rich.console import Console
+from rich.prompt import Prompt, IntPrompt
+from rich.layout import Layout
+import utils
+from rich.theme import Theme
 
-from cli_finance import utils
+custom_theme = Theme({
+    "info": "dim cyan",
+    "warning": "bold yellow",
+    "error": "bold red",
+    "repr.number" :"bold magenta",
+    "repr.string" :"bold yellow"
+})
+console = Console(width=120,markup=True,highlight=True,style='on black',force_terminal=True,theme=custom_theme)
 
-app = typer.Typer()
-console = Console()
+def input_c():
+    category = Prompt.ask("Enter category")
+    type_ = Prompt.ask("Enter transaction type(Income/Expense)")
+    amount = IntPrompt.ask("Enter amount")
+    return category,type_,amount 
 
-
-@app.command()
-def main() -> None:
-    """Console script for cli_finance."""
-    console.print("Replace this message by putting your code into cli_finance.cli.main")
-    console.print("See Typer documentation at https://typer.tiangolo.com/")
-    utils.do_something_useful()
+def main():
+    records = []
+    utils.init_()
+    while True:
+        console.print()
+        type_ = Prompt.ask("Type",choices=['income','expense','show','exit','summary'])
+      
+        if type_ =='summary':
+            rec = utils.get_records()
+            console.print(f"Record count: {len(rec)}")  # debug
+            for r in rec:
+                console.print(dict(r))
+            Prompt.ask("Press Enter to continue")
+        elif type_=='income' or type_=='expense':
+            category ,tp, amount = input_c()
+            utils.add_record(category,tp,amount)
+            console.print(f"[green]Saved:[/green]{category}|{amount}|{type_}")
+        elif type_ =="exit":
+            break
 
 
 if __name__ == "__main__":
-    app()
+    main()
--- src/cli_finance/utils.py
+++ src/cli_finance/utils.py
@@ -1,2 +1,53 @@
-def do_something_useful() -> None:
-    print("Replace this with a utility function")
+import sqlite3
+import pathlib as path
+
+
+
+DB = "records.db"
+def get_conn():
+    con = sqlite3.connect(DB)
+    con.row_factory = sqlite3.Row
+    return con
+def init_():
+    with get_conn() as con:
+        con.execute("""
+        CREATE TABLE IF NOT EXISTS transactions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category VARCHAR NOT NULL,
+        type VARCHAR NOT NULL,
+        amount INTEGER NOT NULL,
+        date TEXT DEFAULT (date('now'))
+         )
+        """)
+def add_record(category,type_,amount):
+    with get_conn() as con:
+        con.execute("INSERT INTO transactions(category,type,amount) VALUES(?,?,?)",(category,type_,amount))
+def get_records():
+    with get_conn() as con:
+        return con.execute("SELECT *FROM transactions").fetchall()
+def delete_all():
+    with get_conn() as con:
+        con.execute("DELETE FROM transactions")
+        con.execute("DELETE FROM sqlite_sequence WHERE name='transactions'")
+def delete_sepcific(id_):
+    with get_conn() as con:
+        con.execute("DELETE FROM transactions WHERE id =?",(id_,))
+# def main():
+#     init_()
+#     add_record('test','income',500)
+#     rec = get_records()
+#     for r in rec:
+#         print(dict(r))
+#     delete_sepcific(1)
+#     print("Specific deleted")
+#     rec = get_records()
+#     for r in rec:
+#         print(dict(r))
+#     rec = get_records()
+#     delete_all()
+#     print("All deleted")
+#     for r in rec:
+#        print(dict(r))
+# main()
+
+
